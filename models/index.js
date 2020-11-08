ba5bf6eab0974621db3314a641eace3100536ee6
shilu <zoushilu@gmail.com>
2020-11-08 15:15:44 -0800

    models updated

--- models/index.js
+++ models/index.js
@@ -1,29 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+ // import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-const sequelize = require('../config/connection.js');
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
-class Category extends Model { }
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-Category.init(
-  {
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-module.exports = Category;
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
